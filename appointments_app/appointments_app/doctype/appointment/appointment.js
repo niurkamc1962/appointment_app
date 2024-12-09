af6af3df08e9bcf8448637b54694bcaedf9c826e
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2024-12-08 20:13:22 -0500

    refactor: better shift management

--- appointments_app/appointments_app/doctype/appointment/appointment.js
+++ appointments_app/appointments_app/doctype/appointment/appointment.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, Hussain and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Appointment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Appointment", {
+	refresh(frm) {
+        frm.set_query('shift', (doc) => {
+            return {
+                filters: {
+                    "clinic": doc.clinic
+                }
+            }
+        })
+	},
+});
