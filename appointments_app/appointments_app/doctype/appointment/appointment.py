e3b68de9f07ecdaf1150d9646e3197ec6e5e9bf6
morancanto <2913001+niurkamc1962@users.noreply.github.com>
2024-12-10 08:41:05 -0500

    fix: handle case when appointment queue doesn't exist

--- appointments_app/appointments_app/doctype/appointment/appointment.py
+++ appointments_app/appointments_app/doctype/appointment/appointment.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2024, Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Appointment(Document):
-	pass
+    def after_insert(self):
+        self.queue_number = self.add_to_appointment_queue()
+        self.save(ignore_permissions=True)
+
+    def add_to_appointment_queue(self):
+        filters = {
+            "date": self.date,
+            "shift": self.shift,
+            "clinic": self.clinic,
+        }
+        appointment_queue_exists = frappe.db.exists(
+            "Appointment Queue",
+            filters,
+        )
+
+        if appointment_queue_exists:
+            q = frappe.get_doc("Appointment Queue", filters)
+        else:
+            q = frappe.new_doc("Appointment Queue")
+            q.update(filters)
+            q.save(ignore_permissions=True)
+
+        q.append("queue", {"appointment": self.name, "status": "Pending"})
+        q.save(ignore_permissions=True)
+
+        return len(q.queue)
